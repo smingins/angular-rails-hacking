2e1e05ff1cfae51e76447e20183d6b09217081b7
Shane Mingins <shane@mingins.com>
2013-05-30 10:51:26 +1200

    Initial Bootstrap

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AngularRailsHacking::Application.config.secret_token = '72e1e7f7830e0f3b3f71b1721042031735e44ddef09595ba2051e3086ce934409034ec941298c58d02f3aad52a913c46c6548d6af722fae35c020d927f955447'
